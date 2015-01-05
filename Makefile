420aea2b9fabb854da8e7775bd0cb5a928bf82b7
Orvar Segerström <orvarsegerstrom@gmail.com>
2015-01-05 16:36:01 +0100

    Added msgpack functionality to localworker

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 CXX_SOURCES = \
 	src/utils.cc \
 	src/protocol/packet.pb.cc
+OTHER_SOURCES = \
+  src/lua/sandbox.h \
+  src/lua/msgpack.h
 
 LUA_CXXFLAGS = -Ilib/LuaJIT-2.0.3/src -Ilib/LuaState-master/include
-LUA_LFLAGS = -Llib/LuaJIT-2.0.3/src -lluajit -ldl
+LUA_LFLAGS = -Llib/LuaJIT-2.0.3/src -lluajit
 
 PROTOBUF_CXXFLAGS = -Ilib/protobuf-2.6.0/src
 PROTOBUF_LFLAGS = -Llib/protobuf-2.6.0/src/.libs -lprotobuf
@@ -12,23 +15,26 @@ BOOST_LFLAGS = -lboost_system -pthread
 
 CXX_OBJECTS = $(CXX_SOURCES:.cc=.o)
 CXXFLAGS = -g -Wall -Werror -Wfatal-errors -Wno-sign-compare -std=c++11 $(PROTOBUF_CXXFLAGS) $(LUA_CXXFLAGS)
-LFLAGS = -static $(BOOST_LFLAGS) $(PROTOBUF_LFLAGS) $(LUA_LFLAGS)
+LFLAGS = -static $(BOOST_LFLAGS) $(PROTOBUF_LFLAGS) $(LUA_LFLAGS) -ldl
 
 all: dist/server dist/nameserver dist/localworker
 
 dist/nameserver: src/nameserver.cc $(CXX_OBJECTS)
 	$(CXX) -o $@ $(CXXFLAGS) $^ $(LFLAGS)
 
-dist/server: src/server.cc $(CXX_OBJECTS)
+dist/server: src/server.cc $(CXX_OBJECTS) $(OTHER_SOURCES)
 	$(CXX) -o $@ $(CXXFLAGS) $^ $(LFLAGS)
 
-dist/localworker: src/localworker.cc $(CXX_OBJECTS)
+dist/localworker: src/localworker.cc $(CXX_OBJECTS) $(OTHER_SOURCES)
 	$(CXX) -o $@ $(CXXFLAGS) $^ $(LFLAGS)
 
 src/protocol/packet.pb.cc: src/protocol/packet.proto
 	cd src/protocol && ../../lib/protobuf-2.6.0/src/protoc --cpp_out=. packet.proto
 
-**/%.o: **/%.cc
+src/lua/%.h: src/lua/%.lua
+	xxd -i $< $@
+
+**/%.o: **/%.cc Makefile
 	$(CXX) $(LUA_CXXFLAGS) $(CXXFLAGS) -c $< -o $@
 
 .PHONY: clean distclean
--- src/localworker.cc
+++ src/localworker.cc
@@ -7,78 +7,22 @@
 
 using namespace std;
 
-const char* const SANDBOX = R"(
-  -- sample sandbox environment
-  sandbox_env = {
-    work = work,
-    ipairs = ipairs,
-    next = next,
-    pairs = pairs,
-    pcall = pcall,
-    tonumber = tonumber,
-    tostring = tostring,
-    type = type,
-    unpack = unpack,
-    coroutine = { create = coroutine.create, resume = coroutine.resume,
-        running = coroutine.running, status = coroutine.status,
-        wrap = coroutine.wrap },
-    string = { byte = string.byte, char = string.char, find = string.find,
-        format = string.format, gmatch = string.gmatch, gsub = string.gsub,
-        len = string.len, lower = string.lower, match = string.match,
-        rep = string.rep, reverse = string.reverse, sub = string.sub,
-        upper = string.upper },
-    table = { insert = table.insert, maxn = table.maxn, remove = table.remove,
-        sort = table.sort },
-    math = { abs = math.abs, acos = math.acos, asin = math.asin,
-        atan = math.atan, atan2 = math.atan2, ceil = math.ceil, cos = math.cos,
-        cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor,
-        fmod = math.fmod, frexp = math.frexp, huge = math.huge,
-        ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max,
-        min = math.min, modf = math.modf, pi = math.pi, pow = math.pow,
-        rad = math.rad, random = math.random, sin = math.sin, sinh = math.sinh,
-        sqrt = math.sqrt, tan = math.tan, tanh = math.tanh },
-    os = { clock = os.clock, difftime = os.difftime, time = os.time },
-  }
-
-
-  function run_sandboxed(f)
-    setfenv(f, sandbox_env)
-    return pcall(f)
-  end
-
-  -- io.write("# code:\n", blueprint, "\n")
-  -- io.write("# work:\n", work, "\n")
-
-  is_error = false
-  error = nil
-  result = "none"
-
-  local f = loadstring(blueprint)
-  if f == nil then
-    is_error = true
-    error = "Syntax error in blueprint"
-    return
-  end
-
-  local ret, e = run_sandboxed(f)
-  if ret then
-    -- TODO: convert table to JSON
-    result = tostring(e)
-  else
-    -- TODO: fuck shit up
-    is_error = true
-    error = tostring(e)
-  end
-)";
+#include "lua/sandbox.h"
+#include "lua/msgpack.h"
 
 std::string runSandboxed(std::string code) {
   lua::State state;
 
   state.setString("blueprint", code);
   state.setInt("work", 1200);
+
   cout << "=== State ready, running ===" << endl;
   try {
-    state.doString(SANDBOX);
+    state.set("malloc", &std::malloc);
+    state.set("realloc", &std::realloc);
+    state.set("free", &std::free);
+    state.doString(std::string(reinterpret_cast<const char*>(src_lua_msgpack_lua)));
+    state.doString(std::string(reinterpret_cast<const char*>(src_lua_sandbox_lua)));
   } catch(const lua::RuntimeError& e) {
     cerr << "Error: " << e.what() << endl;
     return std::string();
